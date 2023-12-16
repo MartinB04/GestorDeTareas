6bca57bff38cc2026c979709f7e2cfb9008e75dd
MartinB04 <martin.bibiano4@outlook.com>
2023-12-15 18:21:04 -0600

    @properties y .setters agregados en clase Usuario
    Se han agregado los properties y .setters para los 3 atributos
    (user, nombre y password)

--- Usuario.py
+++ Usuario.py
@@ -8,8 +8,32 @@ class Usuario:
         self.nombre = nombre
         self.password = password
 
-    def buscarUsuario(self):
+    def buscarUsuario(self,):
         pass
+
+    @property
+    def user(self):
+        return self.user
+    
+    @user.setter
+    def user(self, user):
+        self.user = user
+
+    @property
+    def nombre(self):
+        return self.nombre
+    
+    @nombre.setter
+    def nombre(self, nombre):
+        self.nombre = nombre
+
+    @property
+    def password(self):
+        return self.password
+    
+    @password.setter
+    def password(self, password):
+        self.password = password
 
     def __str__(self) -> str:
         return f"{self.user}\n{self.nombre}\n{self.password}\n"
